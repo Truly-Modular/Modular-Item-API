2b4e9890c9eb530a22c0b7bf340d698b0ec4242b
MartinLaptop <martinbachmor@gmail.com>
2023-07-01 16:24:41 +0200

    -bc fixes

--- common/src/main/java/smartin/miapi/mixin/BCWeaponRegistryMixin.java
+++ common/src/main/java/smartin/miapi/mixin/BCWeaponRegistryMixin.java
@@ -2,26 +2,29 @@ package smartin.miapi.mixin;
 
 
 import net.bettercombat.api.WeaponAttributes;
-import net.bettercombat.logic.WeaponRegistry;
+import net.bettercombat.api.WeaponAttributesHelper;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import smartin.miapi.item.modular.ModularItem;
 import smartin.miapi.modules.properties.compat.BetterCombatHelper;
 
-@Mixin(WeaponRegistry.class)
+@Mixin(WeaponAttributesHelper.class)
 public class BCWeaponRegistryMixin {
     @Inject(
-            method = "getAttributes(Lnet/minecraft/item/ItemStack;)Lnet/bettercombat/api/WeaponAttributes;",
+            method = "Lnet/bettercombat/api/WeaponAttributesHelper;readFromNBT(Lnet/minecraft/item/ItemStack;)Lnet/bettercombat/api/WeaponAttributes;",
             at = @At("HEAD"),
             cancellable = true,
-            remap = false,
+            remap = true,
             require = -1)
     private static void getAttributes(ItemStack itemStack, CallbackInfoReturnable<WeaponAttributes> cir) {
-        WeaponAttributes attributes = BetterCombatHelper.getAttributes(itemStack);
-        if (attributes != null) {
-            cir.setReturnValue(attributes);
+        if(itemStack.getItem() instanceof ModularItem){
+            WeaponAttributes attributes = BetterCombatHelper.getAttributes(itemStack);
+            if (attributes != null) {
+                cir.setReturnValue(attributes);
+            }
         }
     }
 }
