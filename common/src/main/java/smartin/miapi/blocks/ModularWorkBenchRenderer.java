9e0ef926e7468773d8dca2dbc16d938ee8d8dca1
Panda <grames8888@gmail.com>
2023-07-13 18:45:27 -0400

    BER support for non modular items

--- common/src/main/java/smartin/miapi/blocks/ModularWorkBenchRenderer.java
+++ common/src/main/java/smartin/miapi/blocks/ModularWorkBenchRenderer.java
@@ -7,8 +7,12 @@ import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.RangedWeaponItem;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
 import net.minecraft.util.math.RotationAxis;
 import smartin.miapi.client.model.ItemRenderUtil;
+import smartin.miapi.item.modular.ModularItem;
 import smartin.miapi.modules.properties.render.ModelProperty;
 
 public class ModularWorkBenchRenderer implements BlockEntityRenderer<ModularWorkBenchEntity> {
@@ -25,19 +29,34 @@ public class ModularWorkBenchRenderer implements BlockEntityRenderer<ModularWork
 
         matrices.push();
         matrices.translate(8/16f, 11.5/16f, 8/16f);
-        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(be.getCachedState().get(ModularWorkBench.FACING).asRotation()-45));
+        float rotAmnt = be.getCachedState().get(ModularWorkBench.FACING).asRotation();
+        if (stack.getItem() instanceof ModularItem || stack.getItem() instanceof ToolItem || stack.getItem() instanceof SwordItem || stack.getItem() instanceof RangedWeaponItem)
+            rotAmnt-=45;
+        else
+            rotAmnt-=90;
+        matrices.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(rotAmnt));
         matrices.multiply(RotationAxis.POSITIVE_X.rotationDegrees(90));
         matrices.scale(0.75f, 0.75f, 0.75f);
 
-        BakedModel model = ModelProperty.getItemModel(stack);
+        if (stack.getItem() instanceof ModularItem) {
+            BakedModel model = ModelProperty.getItemModel(stack);
 
-        if (model != null)
-            ItemRenderUtil.renderModel(
-                    matrices,
-                    stack, model,
+            if (model != null)
+                ItemRenderUtil.renderModel(
+                        matrices,
+                        stack, model,
+                        ModelTransformationMode.FIXED,
+                        vertexConsumers, light, 0
+                );
+        } else {
+            context.getItemRenderer().renderItem(
+                    stack,
                     ModelTransformationMode.FIXED,
-                    vertexConsumers, light, 0
+                    light, overlay,
+                    matrices, vertexConsumers,
+                    be.getWorld(), 1
             );
+        }
 
         matrices.pop();
     }
