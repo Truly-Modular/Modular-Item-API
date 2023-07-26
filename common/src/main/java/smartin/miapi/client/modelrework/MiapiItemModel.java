d7d02006e0fe21aa6ff4ae96a21561e9e1661d18
Smartin <martinbachmor@gmail.com>
2023-07-26 23:40:51 +0200

    -further model changes

--- common/src/main/java/smartin/miapi/client/modelrework/MiapiItemModel.java
+++ common/src/main/java/smartin/miapi/client/modelrework/MiapiItemModel.java
@@ -1,82 +1,36 @@
 package smartin.miapi.client.modelrework;
 
-import com.mojang.datafixers.util.Pair;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
-import org.joml.Matrix4f;
 import smartin.miapi.item.modular.ModularItem;
 import smartin.miapi.modules.ItemModule;
-import smartin.miapi.modules.properties.SlotProperty;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class MiapiItemModel implements MiapiModel {
-    public List<ModelSupplier> modelSuppliers = new ArrayList<>();
-    List<Pair<Matrix4f, MiapiModel>> models;
-    Map<String, List<Pair<Matrix4f, MiapiModel>>> otherModels;
+public class MiapiItemModel {
+    public static List<ModelSupplier> modelSuppliers = new ArrayList<>();
     final ItemStack stack;
+    final ModuleModel rootModel;
 
     public MiapiItemModel(ItemStack stack) {
         this.stack = stack;
         if (stack.getItem() instanceof ModularItem) {
-            ItemModule.getModules(stack).allSubModules().forEach(module -> {
-                Matrix4f matrix4f = SlotProperty.getLocalTransform(module).toMatrix();
-                for (ModelSupplier supplier : modelSuppliers) {
-                    supplier.getModels(null, module).forEach(model -> {
-                        models.add(new Pair<>(new Matrix4f(matrix4f), model));
-                    });
-                }
-            });
+            rootModel = new ModuleModel(ItemModule.getModules(stack));
+        } else {
+            rootModel = null;
+            throw new RuntimeException("Can only make MiapiModel for Modular Items");
         }
-        models = new ArrayList<>();
-        otherModels = new HashMap<>();
-    }
-
-    private List<Pair<Matrix4f, MiapiModel>> generateModelList(ItemStack stack, String key) {
-        List<Pair<Matrix4f, MiapiModel>> modelList = new ArrayList<>();
-        if (stack.getItem() instanceof ModularItem) {
-            ItemModule.getModules(stack).allSubModules().forEach(module -> {
-                Matrix4f matrix4f = SlotProperty.getLocalTransformStack(module).get(key).toMatrix();
-                for (ModelSupplier supplier : modelSuppliers) {
-                    supplier.getModels(key, module).forEach(model -> {
-                        modelList.add(new Pair<>(new Matrix4f(matrix4f), model));
-                    });
-                }
-            });
-        }
-        return modelList;
     }
 
     public void render(String modelType, MatrixStack matrices, float tickDelta, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        if (!otherModels.containsKey(modelType)) {
-            otherModels.put(modelType, generateModelList(stack, modelType));
-        }
-        otherModels.get(modelType).forEach(matrix4fMiapiModelPair -> {
-            matrices.push();
-            matrices.peek().getPositionMatrix().mul(matrix4fMiapiModelPair.getFirst());
-            matrix4fMiapiModelPair.getSecond().render(matrices, tickDelta, vertexConsumers, light, overlay);
-            matrices.pop();
-        });
+        rootModel.render(modelType, matrices, tickDelta, vertexConsumers, light, overlay);
     }
 
-    @Override
     public void render(MatrixStack matrices, float tickDelta, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        models.forEach(matrix4fMiapiModelPair -> {
-            matrices.push();
-            matrices.peek().getPositionMatrix().mul(matrix4fMiapiModelPair.getFirst());
-            matrix4fMiapiModelPair.getSecond().render(matrices, tickDelta, vertexConsumers, light, overlay);
-            matrices.pop();
-        });
-    }
-
-    @Override
-    public @Nullable Matrix4f subModuleMatrix(int submoduleId) {
-        return null;
+        render(null, matrices, tickDelta, vertexConsumers, light, overlay);
     }
 
     interface ModelSupplier {
--- common/src/main/java/smartin/miapi/client/modelrework/ModuleModel.java
+++ common/src/main/java/smartin/miapi/client/modelrework/ModuleModel.java
@@ -1,14 +1,62 @@
 package smartin.miapi.client.modelrework;
 
 import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
+import smartin.miapi.modules.ItemModule;
+import smartin.miapi.modules.properties.SlotProperty;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ModuleModel {
 
     List<Pair<Matrix4f, MiapiModel>> models;
-
     Map<String, List<Pair<Matrix4f, MiapiModel>>> otherModels;
+    final ItemModule.ModuleInstance instance;
+    Map<Integer, ModuleModel> subModuleModels = new WeakHashMap<>();
+
+    public ModuleModel(ItemModule.ModuleInstance instance) {
+        this.instance = instance;
+        models = generateModel(null);
+    }
+
+    private List<Pair<Matrix4f, MiapiModel>> generateModel(String key) {
+        List<Pair<Matrix4f, MiapiModel>> modelList = new ArrayList<>();
+        Matrix4f matrix4f = SlotProperty.getLocalTransformStack(instance).get(key).toMatrix();
+        for (MiapiItemModel.ModelSupplier supplier : MiapiItemModel.modelSuppliers) {
+            supplier.getModels(key, instance).forEach(model -> {
+                modelList.add(new Pair<>(new Matrix4f(matrix4f), model));
+            });
+        }
+        return modelList;
+    }
+
+    public void render(String modelType, MatrixStack matrices, float tickDelta, VertexConsumerProvider vertexConsumers, int light, int overlay) {
+        if (!otherModels.containsKey(modelType)) {
+            otherModels.put(modelType, generateModel(modelType));
+        }
+        Map<Integer, Matrix4f> map = new HashMap<>();
+        otherModels.get(modelType).forEach(matrix4fMiapiModelPair -> {
+            matrices.push();
+            matrices.peek().getPositionMatrix().mul(matrix4fMiapiModelPair.getFirst());
+            matrix4fMiapiModelPair.getSecond().render(matrices, tickDelta, vertexConsumers, light, overlay);
+            matrices.pop();
+            instance.subModules.forEach((integer, instance1) -> {
+                Matrix4f matrix4f = map.getOrDefault(integer, new Matrix4f());
+                Matrix4f subModuleMatrix = matrix4fMiapiModelPair.getSecond().subModuleMatrix(integer);
+                if (subModuleMatrix != null) {
+                    matrix4f.mul(matrix4fMiapiModelPair.getSecond().subModuleMatrix(integer));
+                }
+                map.put(integer, matrix4f);
+            });
+        });
+        instance.subModules.forEach((integer, instance1) -> {
+            matrices.push();
+            matrices.multiplyPositionMatrix(map.get(integer));
+            ModuleModel subModuleModel = subModuleModels.getOrDefault(integer, new ModuleModel(instance1));
+            subModuleModel.render(modelType, matrices, tickDelta, vertexConsumers, integer, overlay);
+            matrices.pop();
+        });
+    }
 }
