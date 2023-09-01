7f47c04942b7640c7c429ddc9d6058784480e476
Smartin <martinbachmor@gmail.com>
2023-09-01 18:26:47 +0200

    - enchantment table fix

--- common/src/main/java/smartin/miapi/mixin/EnchantmentHelperMixin.java
+++ common/src/main/java/smartin/miapi/mixin/EnchantmentHelperMixin.java
@@ -4,14 +4,17 @@ import com.google.common.collect.Lists;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
+import net.minecraft.util.Util;
+import net.minecraft.util.collection.Weighting;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import smartin.miapi.Miapi;
 import smartin.miapi.item.modular.ModularItem;
 import smartin.miapi.modules.properties.ChannelingProperty;
 
@@ -65,18 +68,42 @@ public class EnchantmentHelperMixin {
     }
 
     @Inject(method = "Lnet/minecraft/enchantment/EnchantmentHelper;generateEnchantments(Lnet/minecraft/util/math/random/Random;Lnet/minecraft/item/ItemStack;IZ)Ljava/util/List;", at = @At("HEAD"), cancellable = true)
-    private static void miapi$ModifyPossibleEntries(Random random, ItemStack stack, int level, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
-        Miapi.LOGGER.info("Modular Item Enchanting check - 1");
+    private static void miapi$modifyGenerateEnchantments(Random random, ItemStack stack, int level, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
+        if(stack.getItem() instanceof ModularItem){
+            ArrayList<EnchantmentLevelEntry> list = Lists.newArrayList();
+            Item item = stack.getItem();
+            int i = item.getEnchantability();
+            if (i <= 0) {
+                cir.setReturnValue(list);
+            }
+            level += 1 + random.nextInt(i / 4 + 1) + random.nextInt(i / 4 + 1);
+            float f = (random.nextFloat() + random.nextFloat() - 1.0f) * 0.15f;
+            List<EnchantmentLevelEntry> list2 = getLevels(level = MathHelper.clamp(Math.round((float)level + (float)level * f), 1, Integer.MAX_VALUE), stack, treasureAllowed);
+            if (!list2.isEmpty()) {
+                Weighting.getRandom(random, list2).ifPresent(list::add);
+                while (random.nextInt(50) <= level) {
+                    if (!list.isEmpty()) {
+                        EnchantmentHelper.removeConflicts(list2, Util.getLast(list));
+                    }
+                    if (list2.isEmpty()) break;
+                    Weighting.getRandom(random, list2).ifPresent(list::add);
+                    level /= 2;
+                }
+            }
+            cir.setReturnValue(list);
+        }
     }
 
-    @Inject(method = "Lnet/minecraft/enchantment/EnchantmentHelper;getPossibleEntries(ILnet/minecraft/item/ItemStack;Z)Ljava/util/List;", at = @At("HEAD"), cancellable = true)
-    private static void miapi$ModifyPossibleEntries(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
-        Miapi.LOGGER.info("Modular Item Enchanting check - 2");
+    @Inject(
+            method = "Lnet/minecraft/enchantment/EnchantmentHelper;getPossibleEntries(ILnet/minecraft/item/ItemStack;Z)Ljava/util/List;",
+            at = @At("HEAD"),
+            cancellable = true)
+    private static void miapi$modifyPossibleEntries(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
         if(stack.getItem() instanceof ModularItem){
-            Miapi.LOGGER.info("Modular Item Enchanting check - 3");
             ArrayList<EnchantmentLevelEntry> list = Lists.newArrayList();
             for (Enchantment enchantment : Registries.ENCHANTMENT) {
-                if ( (!enchantment.isTreasure() || enchantment.isTreasure() && treasureAllowed) &&
+                if (
+                        (!enchantment.isTreasure() || enchantment.isTreasure() && treasureAllowed) &&
                         enchantment.isAvailableForRandomSelection() && enchantment.isAcceptableItem(stack))
                 {
                     for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
@@ -87,5 +114,23 @@ public class EnchantmentHelperMixin {
             }
             cir.setReturnValue(list);
         }
+    }
+
+    private static List<EnchantmentLevelEntry> getLevels(int power, ItemStack stack, boolean treasureAllowed) {
+        ArrayList<EnchantmentLevelEntry> list = Lists.newArrayList();
+        if(stack.getItem() instanceof ModularItem){
+            for (Enchantment enchantment : Registries.ENCHANTMENT) {
+                if (
+                        (!enchantment.isTreasure() || enchantment.isTreasure() && treasureAllowed) &&
+                                enchantment.isAvailableForRandomSelection() && enchantment.isAcceptableItem(stack))
+                {
+                    for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+                        if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) continue;
+                        list.add(new EnchantmentLevelEntry(enchantment, i));
+                    }
+                }
+            }
+        }
+        return list;
     }
 }
