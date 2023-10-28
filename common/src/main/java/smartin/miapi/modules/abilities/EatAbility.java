7f25911acf243b2d88503bc4b89912897bd00fe6
Panda <grames8888@gmail.com>
2023-10-27 21:07:00 -0400

    eat ability

---
+++ common/src/main/java/smartin/miapi/modules/abilities/EatAbility.java
@@ -0,0 +1,75 @@
+package smartin.miapi.modules.abilities;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+import smartin.miapi.modules.abilities.util.ItemAbilityManager;
+import smartin.miapi.modules.abilities.util.ItemUseAbility;
+import smartin.miapi.modules.properties.EdibleProperty;
+
+/**
+ * Allows items to be eaten, use in conjunction with {@link EdibleProperty}
+ */
+public class EatAbility implements ItemUseAbility {
+    public static final String KEY = "eat";
+
+    public EatAbility() {}
+
+    @Override
+    public boolean allowedOnItem(ItemStack itemStack, World world, PlayerEntity player, Hand hand, ItemAbilityManager.AbilityContext abilityContext) {
+        EdibleProperty.Holder data = EdibleProperty.get(itemStack);
+        return data != null && (data.alwaysEdible || player.getHungerManager().isNotFull());
+    }
+
+    @Override
+    public UseAction getUseAction(ItemStack itemStack) {
+        return UseAction.EAT;
+    }
+
+    @Override
+    public int getMaxUseTime(ItemStack itemStack) {
+        return (int) (32*EdibleProperty.get(itemStack).eatingSpeed);
+    }
+
+    public int getMaxUseTime(EdibleProperty.Holder data) {
+        return (int) (32*data.eatingSpeed);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (user.getItemCooldownManager().isCoolingDown(user.getStackInHand(hand).getItem())) {
+            return TypedActionResult.pass(user.getStackInHand(hand));
+        }
+
+        user.setCurrentHand(hand);
+        return TypedActionResult.consume(user.getStackInHand(hand));
+    }
+
+    @Override
+    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        if (remainingUseTicks <= 0) {
+            user.stopUsingItem();
+            EdibleProperty.Holder data = EdibleProperty.get(stack);
+
+            boolean isClient = user.getWorld().isClient;
+            if (isClient) {
+                user.getWorld().playSound(user.getX(), user.getY(), user.getZ(), user.getEatSound(stack), SoundCategory.NEUTRAL, 1, 1.0f + (world.random.nextFloat() - world.random.nextFloat()) * 0.4f, true);
+            }
+            if (user instanceof PlayerEntity player) {
+                if (!isClient && data != null) {
+                    player.getHungerManager().add(data.hunger, (float) data.saturation);
+                    data.effects.forEach(e -> player.addStatusEffect(new StatusEffectInstance(e)));
+                } else {
+                    user.getWorld().playSound(user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_PLAYER_BURP, SoundCategory.NEUTRAL, 0.5f, world.random.nextFloat() * 0.1f + 0.9f, true);
+                }
+            }
+        }
+    }
+}
---
+++ common/src/main/java/smartin/miapi/modules/properties/EdibleProperty.java
@@ -0,0 +1,138 @@
+package smartin.miapi.modules.properties;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.JsonOps;
+import com.redpxnda.nucleus.codec.AutoCodec;
+import com.redpxnda.nucleus.util.JsonUtil;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import smartin.miapi.Miapi;
+import smartin.miapi.item.modular.StatResolver;
+import smartin.miapi.modules.ItemModule;
+import smartin.miapi.modules.cache.ModularItemCache;
+import smartin.miapi.modules.properties.util.MergeType;
+import smartin.miapi.modules.properties.util.ModuleProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EdibleProperty implements ModuleProperty {
+    public static final String KEY = "edible";
+    public static EdibleProperty property;
+
+    public EdibleProperty() {
+        property = this;
+        ModularItemCache.setSupplier(KEY, EdibleProperty::createCache);
+    }
+
+    public static Holder get(ItemStack itemStack) {
+        return (Holder) ModularItemCache.getRaw(itemStack, KEY);
+    }
+
+    public static Holder createCache(ItemStack stack) {
+        Holder result = new Holder(0, 0, 1, false, new ArrayList<>());
+        for (ItemModule.ModuleInstance subModule : ItemModule.getModules(stack).allSubModules()) {
+            JsonElement element = subModule.getProperties().get(property);
+            if (element == null) continue;
+
+            Holder holder = RawData.codec.parse(JsonOps.INSTANCE, element).getOrThrow(false, s -> {
+                Miapi.LOGGER.error("Failed to decode using codec during cache creation for a CodecBasedProperty! -> " + s);
+            }).toHolder(subModule);
+
+            result.hunger+=holder.hunger;
+            result.saturation+=holder.saturation;
+            result.eatingSpeed*=holder.eatingSpeed;
+            result.effects.addAll(holder.effects);
+            if (holder.alwaysEdible) result.alwaysEdible = true;
+        }
+
+        return result;
+    }
+
+    @Override
+    public boolean load(String moduleKey, JsonElement data) throws Exception {
+        RawData.codec.parse(JsonOps.INSTANCE, data).getOrThrow(false, s -> {
+            Miapi.LOGGER.error("Failed to parse data for edible property for module with key '{}'! -> {}", moduleKey, s);
+            throw new RuntimeException();
+        });
+        return true;
+    }
+
+    @Override
+    public JsonElement merge(JsonElement oldEl, JsonElement toMergeEl, MergeType type) {
+        if (type == MergeType.OVERWRITE) return toMergeEl.deepCopy();
+
+        JsonObject newElement = new JsonObject();
+        JsonObject old = oldEl.getAsJsonObject();
+        JsonObject toMerge = toMergeEl.getAsJsonObject();
+
+        newElement.addProperty("hunger", JsonUtil.getOrElse(old, "hunger", "0") + "+" + JsonUtil.getOrElse(toMerge, "hunger", "0"));
+        newElement.addProperty("saturation", JsonUtil.getOrElse(old, "saturation", "0") + "+" + JsonUtil.getOrElse(toMerge, "saturation", "0"));
+        newElement.addProperty("eatingSpeed", JsonUtil.getOrElse(old, "eatingSpeed", "0") + "+" + JsonUtil.getOrElse(toMerge, "eatingSpeed", "0"));
+
+        JsonElement oldEdibility = old.get("alwaysEdible");
+        JsonElement toMergeEdibility = toMerge.get("alwaysEdible");
+        newElement.addProperty("alwaysEdible", (oldEdibility != null && oldEdibility.getAsBoolean()) || (toMergeEdibility != null && toMergeEdibility.getAsBoolean()));
+
+        JsonElement oldEffects = old.get("effects");
+        JsonElement toMergeEffects = toMerge.get("effects");
+        JsonArray newEffects = new JsonArray();
+        if (oldEffects != null) newEffects.addAll(oldEffects.getAsJsonArray());
+        if (toMergeEffects != null) newEffects.addAll(toMergeEffects.getAsJsonArray());
+        newElement.add("effects", newEffects);
+
+        return newElement;
+    }
+
+    public static class RawData {
+        public static final Codec<RawData> codec = AutoCodec.of(RawData.class).codec();
+
+        public StatResolver.IntegerFromStat hunger; // todo perhaps make these number values similar to DoubleProperty's operation?
+        public StatResolver.DoubleFromStat saturation;
+        public @AutoCodec.Optional StatResolver.DoubleFromStat eatingSpeed = new StatResolver.DoubleFromStat(1);
+        public @AutoCodec.Optional boolean alwaysEdible = false;
+        public @AutoCodec.Optional List<StatusEffectHolder> effects = new ArrayList<>();
+
+        public Holder toHolder(ItemModule.ModuleInstance instance) {
+            return new Holder(
+                    hunger.evaluate(instance),
+                    saturation.evaluate(instance),
+                    eatingSpeed.evaluate(instance),
+                    alwaysEdible,
+                    effects.stream().map(e -> new StatusEffectInstance(e.effect, e.duration, e.amplifier, e.ambient, e.showParticles, e.showIcon)).toList());
+        }
+    }
+
+    public static final class Holder {
+        public int hunger;
+        public double saturation;
+        public double eatingSpeed;
+        public boolean alwaysEdible;
+        public List<StatusEffectInstance> effects;
+
+        public Holder(int hunger, double saturation, double eatingSpeed, boolean alwaysEdible, List<StatusEffectInstance> effects) {
+            this.hunger = hunger;
+            this.saturation = saturation;
+            this.eatingSpeed = eatingSpeed;
+            this.alwaysEdible = alwaysEdible;
+            this.effects = effects;
+        }
+    }
+
+    @AutoCodec.Override("codec")
+    @AutoCodec.Settings(optionalByDefault = true)
+    public static class StatusEffectHolder {
+        public static final Codec<StatusEffectHolder> codec = AutoCodec.of(StatusEffectHolder.class).codec();
+
+        public @AutoCodec.Mandatory StatusEffect effect;
+        public @AutoCodec.Mandatory int duration;
+        public @AutoCodec.Mandatory int amplifier;
+        public boolean ambient = false;
+        public boolean showParticles = true;
+        public boolean showIcon = true;
+    }
+}
