510be050bc8ea739c3a836eb39bc2a089d107fc4
Smartin_b <martinbachmor@gmail.com>
2024-02-09 17:23:13 +0100

    - gradle fixes

--- common/src/main/java/smartin/miapi/item/FakeEnchantment.java
+++ common/src/main/java/smartin/miapi/item/FakeEnchantment.java
@@ -4,7 +4,6 @@ import net.minecraft.enchantment.Enchantment;
 import net.minecraft.item.ItemStack;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
