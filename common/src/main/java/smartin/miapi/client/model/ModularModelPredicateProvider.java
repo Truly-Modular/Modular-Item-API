17ac441447514eabdf8e2ce68085001ebdb6a15d
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-01-15 17:13:21 +0100

    - re-enable model predicates

--- common/src/main/java/smartin/miapi/client/model/ModularModelPredicateProvider.java
+++ common/src/main/java/smartin/miapi/client/model/ModularModelPredicateProvider.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.item.ClampedModelPredicateProvider;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
+import smartin.miapi.mixin.client.ModelPredicateProviderRegistryAccessor;
 import smartin.miapi.registries.RegistryInventory;
 
 @Environment(EnvType.CLIENT)
@@ -15,16 +16,16 @@ public class ModularModelPredicateProvider {
 
     public static void registerModularModelOverride(Identifier id, ClampedModelPredicateProvider provider) {
         RegistryInventory.addCallback(RegistryInventory.modularItems, item -> {
-            //ModelPredicateProviderRegistryAccessor.register(item, id, provider);
+            ModelPredicateProviderRegistryAccessor.register(item, id, provider);
         });
     }
 
     public static void registerModelOverride(Item item, Identifier id, ClampedModelPredicateProvider provider) {
-        //ModelPredicateProviderRegistryAccessor.register(item, id, provider);
+        ModelPredicateProviderRegistryAccessor.register(item, id, provider);
     }
 
     public static void registerModularItemModelOverride(Identifier identifier, Identifier id, ClampedModelPredicateProvider provider) {
         Item item = RegistryInventory.modularItems.get(identifier);
-        //ModelPredicateProviderRegistryAccessor.register(item, id, provider);
+        ModelPredicateProviderRegistryAccessor.register(item, id, provider);
     }
 }
