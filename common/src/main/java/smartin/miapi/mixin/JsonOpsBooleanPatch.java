c7ff5ffcd767b91560378cf57dbfad2f9851242a
Smartin_b <67115074+smartin-b@users.noreply.github.com>
2025-12-29 17:08:44 +0100

    - add patch to allow for number to boolean json behaviour

---
+++ common/src/main/java/smartin/miapi/mixin/JsonOpsBooleanPatch.java
@@ -0,0 +1,36 @@
+package smartin.miapi.mixin;
+
+import com.google.gson.JsonElement;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.JsonOps;
+import com.mojang.serialization.Lifecycle;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(JsonOps.class)
+public abstract class JsonOpsBooleanPatch {
+    @Shadow
+    public abstract JsonElement remove(JsonElement input, String key);
+
+    @ModifyReturnValue(
+            method = "Lcom/mojang/serialization/JsonOps;getBooleanValue(Lcom/google/gson/JsonElement;)Lcom/mojang/serialization/DataResult;",
+            at = @At("RETURN"),
+            remap = true,
+            require = -1)
+    private DataResult<Boolean> miapi$allowForNBTWeirdnessConversion(DataResult<Boolean> original, final JsonElement input) {
+        if (original.isError()) {
+            if (input != null && input.isJsonPrimitive() && input.getAsJsonPrimitive().isNumber()) {
+                int number = input.getAsInt();
+                if (number == 0) {
+                    return new DataResult.Success<>(false, Lifecycle.stable());
+                }
+                if (number == 1) {
+                    return new DataResult.Success<>(true, Lifecycle.stable());
+                }
+            }
+        }
+        return original;
+    }
+}
